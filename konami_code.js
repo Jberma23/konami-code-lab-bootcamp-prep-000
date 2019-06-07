40098a2916a056626b3088ad92d0fd651f1fd3e9
Jesse Berman <jesse.ben.berman@gmail.com>
2019-06-07 15:13:16 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -13,21 +13,17 @@ const codes = [
 function init(){
 let index = 0;
 
-  const input = document.body.addEventListener('keydown', function(event)){
-
-function onKeyDownHandler(event){
-  const key = e.key;
-
-  if (key === codes[index]){
+  const input = document.body
+input.addEventListener(`keydown`, function(e) {
+  if (e.key === codes[index]){
     index++;
 
     if (index === codes.length){
-      alert("Hurray!");
+      alert("Congratulations!");
       index = 0;
     }
   } else {
     index = 0;
   }
-}
 }
 }
