9275aa4050a05ddfd649fa64ed2e13a390649b30
Jesse Berman <jesse.ben.berman@gmail.com>
2019-06-07 15:08:33 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -10,23 +10,44 @@ const codes = [
   "b",
   "a"
 ];
-function init(){
+let i = 0
+
+ function init() {	function init() {
+  // your code here	  // your code here
+  //const input = document.querySelector('input');
+ /*
+  document.addEventListener('keydown', function(e) {
+    console.log(e.key);
+  });
+*/
+//document.addEventListener('keydown', onKeyDownHandler(e))
+  document.body.addEventListener('keydown', function(e) {
+    i++
+    console.log(i)
+
+     console.log(e.key)
+    onKeyDownHandler(e);
+  });
+
+ }
+
+ // Keep track of index outside of the event handler.
 let index = 0;
 
-  document.body.addEventListener('keydown', onKeyDownHandler(e))
-
-function onKeyDownHandler(e){
+ // This is the function that would be invoked by the event listener.
+function onKeyDownHandler(e) {
   const key = e.key;
-  for (let i = 0, l = codes.length; i < l; i++) {
-    triggerKeyDown(codes[i]);
-  if (key === codes[index]){
-    index++;
 
-    if (index === codes.length){
+   if (key === codes[index]) {
+    index++;
+    console.log(index)
+
+     if (index === codes.length) {
       alert("Hurray!");
-      index = 0;
+
+       index = 0;
     }
   } else {
     index = 0;
   }
-}
+}	}
